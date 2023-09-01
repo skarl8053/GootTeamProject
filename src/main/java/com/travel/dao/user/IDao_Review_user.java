48934db4984608ff1bddc0e215cba63b7a6be316
HeiKayYoung <kyoungmihaha@gmail.com>
2023-09-01 15:17:37 +0900

    review_change_file_upload

--- src/main/java/com/travel/dao/user/IDao_Review_user.java
+++ src/main/java/com/travel/dao/user/IDao_Review_user.java
@@ -9,6 +9,9 @@ import com.travel.dto.user.DTO_Review_user;
 public interface IDao_Review_user {
 
 	public void rwrite(String m_no, String s_no, String r_no, String photo_url,String review_rate, String review_content);
+	
+	public void updateAvgRate(String s_no);
+
 	public ArrayList<DTO_Review_user> rlist();
 	public ArrayList<DTO_Review_user> rlistP();
 	public void rReport(String review_no);
